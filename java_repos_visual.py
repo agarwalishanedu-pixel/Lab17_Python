caab2d695190c909fea8c7789cb59946775511c2
Ishan <agarwal.ishanedu@gmail.com>
2026-05-15 23:23:24 -0400

    initial set up with api call

--- java_repos_visual.py
+++ java_repos_visual.py
@@ -6,3 +6,14 @@ Starter code: NA
 Date: May 15, 2026
 """
 
+import requests
+import plotly.express as px
+
+#API Call url
+url = "https://api.github.com/search/repositories"
+url += "?q=language:java+sort:stars+stars:>10000"
+
+headers = {"Accept": "application/vnd.github.v3+json"}
+response = requests.get(url, headers=headers)
+
+print(f'Status Code: {response.status_code}')
